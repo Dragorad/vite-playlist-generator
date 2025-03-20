aba821d79c45105c2f6f46bbac5de90c1ad4203c
Dragorad <mi.e.drago@gmail.com>
2025-03-20 16:18:20 +0200

    api key

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // Firebase конфигурация
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_USER_FIREBASE_API_KEY,
+  apiKey: 'AIzaSyDPJwgJoUmBUWhYW7vL5DisG1fb-tpWKJI',
   authDomain: import.meta.env.VITE_USER_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_USER_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_USER_FIREBASE_STORAGE_BUCKET,
