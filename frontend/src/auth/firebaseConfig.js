bca43a2f3d4afbfa46eaf0605f85b4ca0a9a6e42
Dragora <mi.e.drago@gmail.com>
2026-03-08 23:12:17 +0200

    User User

--- frontend/src/auth/firebaseConfig.js
+++ frontend/src/auth/firebaseConfig.js
@@ -2,9 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_USER_USER_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_USER_USER_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_USER_USER_FIREBASE_PROJECT_ID,
+  apiKey: import.meta.env.VITE_USER_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_USER_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_USER_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_USER_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_USER_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_USER_FIREBASE_APP_ID
